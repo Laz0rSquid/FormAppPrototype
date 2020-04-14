98d2bdde0ebe8dc1222ed3456f669de5c1451923
Marcus Kopp <marcus.kopp86@gmail.com>
2020-04-14 13:58:03 +0200

    Added components/DynamicForm, created index.js and form.css

---
+++ react-dynamicform/src/components/DynamicForm/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './form.css';
+
+export default class DynamicForm extends Component {
+    state = {
+
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    render()
+}
